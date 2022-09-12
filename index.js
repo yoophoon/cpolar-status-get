2b28ce5de03a63c44c18f79e6745754913df9de5
yoophoon <2420074212@qq.com>
2022-09-12 20:39:17 +0800

    modified:   index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const isDebugger = false;
 const start = () => {
   return new Promise(async (resolve, reject) => {
     try {
-      const browser = await puppeteer.launch({ headless: false });
+      const browser = await puppeteer.launch();
       const page = await browser.newPage();
       await page.setViewport({ width: 1920, height: 1080 });
       await page.goto('https://www.cpolar.com/', { timeout: 0 });
